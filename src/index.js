2eb1257152cbc098a8486024915f2222b7192fef
Linlin Li <paris1900lll@gmail.com>
2021-05-06 15:30:14 +0100

    fghj

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 console.log(data);
 // We create individual components here
+// - You'll find a variable called data in the console.log. That's your list of dogs
+// - Render the top list of dogs using the list item template you'll find on the HTML file
+// - Each list item should be clickable. When you click on an item, the selected dog should display on the main card
+// - The main card should contain all the information from the selected dog. Follow the template for the main card that you'll find on the HTML file.
+// - There should be only one card at the time on the screen
+// - The card should have a button that toggles for the selected dog between good dog/ bad dog
+
+// Tips
+// - Take advantage of scope in JS to have access to the data you need
+// - Remember you can add event listeners to any element on the page
+
+// Challenge
+// You might have noticed there's a plus button at the beginning of the top row. Add the behaviour to it. When clicked, it should replace the main card with a form to add a new dog to the list. 
+// You'll find a template for the form on the HTML page. Once the form is submitted, add the new dog to the beginning of the list, right next to the plus button.
