832755b90cd0179178eb2fac8542bbab926b8f2b
Linlin Li <paris1900lll@gmail.com>
2021-05-07 01:02:02 +0100

    last change

--- src/index.js
+++ src/index.js
@@ -71,18 +71,97 @@ for (let i = 0; i<data.length; i++){
 
     ulEl.append(liEl )
 }
+let linewEl = document.querySelector('.dogs-list__button--add')
 
+linewEl.addEventListener('click', function (){
 
+    let sectionEl = document.querySelector('.main__dog-section')
+    sectionEl.innerText = ''
+    let h2El = document.createElement('h2')
+    h2El.innerText = 'Add a new Dog'
+    let formEl = document.createElement('form')
+    formEl.setAttribute('class','form')
 
+    let nameLabel = document.createElement('label')
+    nameLabel.innerText = "Dog's name"
+    nameLabel.setAttribute('for','name')
+    let nameinput = document.createElement('input')
+    nameinput.setAttribute('type', 'text')
+    nameinput.setAttribute('id', 'name')
+    nameinput.setAttribute('name', 'name')
+    nameinput.setAttribute('placeholder', ' put dog name')
 
-// {
-//     id: 1,
-//     name: "Mr. Bonkers",
-//     bio: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum,
-//             minima voluptates libero cumque rerum consequatur optio aliquid sint
-//             eum maxime illo laborum omnis quo ab rem cupiditate nulla
-//             perspiciatis ipsum!`,
-//     isGoodDog: true,
-//     image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_1.jpg"
-//   }
+    let imageLabel = document.createElement('label')
+    imageLabel.innerText = "Dog's picture"
+    imageLabel.setAttribute('for','image')
+    let imageEl = document.createElement('input')
+    imageEl.setAttribute('type','url')
+    imageEl.setAttribute('id','image')
+    imageEl.setAttribute('name','image')
 
+    let biolabel = document.createElement('label')
+    biolabel.innerText = "Dog's bio"
+    biolabel.setAttribute('for','bio')
+    let textEl = document.createElement('textarea')
+    textEl.setAttribute('rows','5')
+    textEl.setAttribute('id','bio')
+    textEl.setAttribute('name','bio')
+
+    let submitInput = document.createElement('input')
+    submitInput.setAttribute('type','submit')
+    submitInput.setAttribute('id','submit')
+    submitInput.setAttribute('name','submit')
+    submitInput.setAttribute('value',"Let's add a dog!")
+    submitInput.setAttribute('class','form__button')
+    formEl.append(nameLabel, nameinput, imageLabel,imageEl, biolabel, textEl,submitInput )
+    sectionEl .append(h2El,formEl)
+
+    formEl.addEventListener('submit', function (event){
+        event.preventDefault()
+        let liEl = document.createElement('li')
+        liEl.setAttribute('class', 'dogs-list__button')
+        
+        liEl.innerText = formEl.name.value
+
+        liEl.addEventListener('click', function(){
+            let sectionEl = document.querySelector('.main__dog-section')
+            sectionEl.innerText = ''
+            let h2El = document.createElement('h2')
+          
+            h2El.innerText = formEl.name.value
+            let imgEl = document.createElement('img')
+            
+            imgEl.setAttribute('src', formEl.image.value)
+            imgEl.setAttribute('style','width:400px; height:300px;')
+            let biodivEl = document.createElement('div')
+            let h3El = document.createElement('h3')
+            h3El.innerText = 'bio'
+            let pinfoEl = document.createElement('p')
+            pinfoEl.innerText = formEl.bio.value
+            let divconfimrEl = document.createElement('div')
+            divconfimrEl.setAttribute('class', 'main__dog-section__btn')
+            let emEl = document.createElement('em')
+            emEl.innerText = 'Is naughty?'
+            let pconfirmEl = document.createElement('p')
+            let btbdog = document.createElement('button')
+            
+            pconfirmEl.innerText = ' yes'
+            btbdog.innerText = 'good dog'
+            btbdog.addEventListener('click', function (){
+                btbdog.innerText = 'bad dog'
+                pconfirmEl.innerText =' no'
+                pconfirmEl.prepend(emEl) 
+                }
+            
+            )
+            sectionEl.append(h2El,imgEl,biodivEl,divconfimrEl)
+            biodivEl.append(h3El,pinfoEl)
+            divconfimrEl.append(pconfirmEl,btbdog)
+            pconfirmEl.prepend(emEl)
+            
+    })
+    ulEl.append(liEl)
+    formEl.reset()
+}) 
+
+})
